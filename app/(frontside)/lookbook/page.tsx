67ee3a47bf36466fddecafa6fd8b5ce9484de344
Alex Kadai <alexandrkadai@gmail.com>
2024-11-04 20:36:49 +0200

    LookBook

--- app/(frontside)/lookbook/page.tsx
+++ app/(frontside)/lookbook/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+import im1 from '@/app/assets/testimage/IMG_1201.jpeg';
 export default function page() {
-  return <div>Hello from the LookBook Page</div>;
+  return (
+    <div className="flex flex-col mt-10 gap-10 justify-center items-center">
+      <Image src={im1} width={350} height={350} alt="lookbok" />
+      <Image src={im1} width={350} height={350} alt="lookbok" />
+      <Image src={im1} width={350} height={350} alt="lookbok" />
+      <Image src={im1} width={350} height={350} alt="lookbok" />
+    </div>
+  );
 }
