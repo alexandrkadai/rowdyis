aa2bbd82a15ecd5171bed8c2925377d1c0f1a986
Alex Kadai <alexandrkadai@gmail.com>
2025-02-23 00:13:50 +0200

    Get userid Cookies refactoring in getcard coponnet

--- app/components/frontside/GetCart.tsx
+++ app/components/frontside/GetCart.tsx
@@ -3,25 +3,30 @@ import { redis } from '@/app/lib/redis';
 import { getUserId } from '@/app/lib/userClaude';
 
 export default async function GetCart() {
-  const userID = await getUserId();
+  try {
+    const userID = await getUserId();
 
-  if (!userID) {
-    throw new Error('User ID is not set');
+    if (!userID) {
+      throw new Error('User ID is not set');
+    }
+
+    const cart: iCart | null = await redis.get(`cart-${userID}`);
+
+    if (cart !== null && Array.isArray(cart.items)) {
+      const itemsCart = cart.items;
+      const total = itemsCart.reduce(
+        (sum, item) => sum + (item?.quantity || 0),
+        0
+      );
+      const totalPrice = itemsCart.reduce(
+        (sum, item) => sum + (item?.quantity || 0) * (item?.price || 0),
+        0
+      );
+      return { itemsCart, total, totalPrice };
+    }
+    return { itemsCart: [], total: 0, totalPrice: 0 };
+  } catch (error) {
+    console.error('Error retrieving cart:', error);
+    return { itemsCart: [], total: 0, totalPrice: 0 };
   }
-
-  const cart: iCart | null = await redis.get(`cart-${userID}`);
-
-  if (cart !== null && Array.isArray(cart.items)) {
-    const itemsCart = cart.items;
-    const total = itemsCart.reduce(
-      (sum, item) => sum + (item?.quantity || 0),
-      0
-    );
-    const totalPrice = itemsCart.reduce(
-      (sum, item) => sum + (item?.quantity || 0) * (item?.price || 0),
-      0
-    );
-    return { itemsCart, total, totalPrice };
-  }
-  return { itemsCart: [], total: 0, totalPrice: 0 };
 }
