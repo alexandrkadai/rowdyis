73ce8eb510c46e145286e4717393b7716a69ebf0
Alex Kadai <alexandrkadai@gmail.com>
2025-02-23 00:24:08 +0200

    animation componnet refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -4,73 +4,50 @@ import Image from 'next/image';
 import sleep from '@/app/assets/animation/RowdYSleep.gif';
 import woke from '@/app/assets/animation/RowdYWoke.gif';
 import { Button } from '@/components/ui/button';
-import { Suspense, useState } from 'react';
-import { redirect } from 'next/navigation';
-import { Skeleton } from '@/components/ui/skeleton';
-import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const [wokes, setWokes] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
   if (wokes === true) {
     setTimeout(() => {
-      redirect('/shop');
+      router.push('/shop');
     }, 4000);
   }
 
   return (
-    <Suspense fallback={<MainLoadingScreen />}>
-      <div
-        className="w-full h-[100vh] relative flex text-center justify-center cursor-[url(cursor.cur),_pointer] bg-[#fcfcff] "
-        onClick={() => setWokes(true)}>
-        <div className="absolute top-28 left-0 right-0 m-auto text-center">
-          {isLoading && (
-            <div className="flex flex-col items-center gap-4 justify-center">
-              
-              <span className="font-bold text-lg">Loading ...</span>
-              <Loader2 className="animate-spin" />
-            </div>
-          )}
-          <>
-            <Image
-              src={sleep}
-              width={100}
-              height={100}
-              alt="animation sleep"
-              priority
-              className={cn(
-                wokes ? 'opacity-0' : 'opacity-100',
-                isLoading ? 'opacity-0' : 'opacity-100',
-                'absolute top-10 left-0 right-0 m-auto text-center w-96 transition-opacity duration-300',
-              )}
-              onLoad={() => setIsLoading(false)}
-            />
-            <Image
-              src={woke}
-              width={100}
-              height={100}
-              alt="animation woke"
-              className={cn(
-                wokes ? 'opacity-100' : 'opacity-0',
-                'absolute top-10 left-0 right-0 m-auto text-center w-96 transition-opacity duration-300',
-              )}
-            />
-          </>
-        </div>
-        <Button className="uppercase absolute bottom-[20%]  m-auto" onClick={() => setWokes(true)}>
-          Start
-        </Button>
-      </div>
-    </Suspense>
-  );
-}
-
-function MainLoadingScreen() {
-  return (
-    <div className="w-full h-full ">
+    <div
+      className="w-full h-[100vh] relative flex text-center justify-center cursor-[url(cursor.cur),_pointer] bg-[#fcfcff]">
       <div className="absolute top-28 left-0 right-0 m-auto text-center">
-        <Skeleton className="w-[100px] h-[100px]" />
+        <Image
+          src={sleep}
+          width={100}
+          height={100}
+          alt="animation sleep"
+          priority
+          className={cn(
+            wokes ? 'opacity-0' : 'opacity-100',
+            'absolute top-10 left-0 right-0 m-auto text-center w-96 transition-opacity duration-300',
+          )}
+        />
+        <Image
+          src={woke}
+          width={100}
+          height={100}
+          alt="animation woke"
+          className={cn(
+            wokes ? 'opacity-100' : 'opacity-0',
+            'absolute top-10 left-0 right-0 m-auto text-center w-96 transition-opacity duration-300',
+          )}
+        />
       </div>
+      <Button
+        className="uppercase absolute bottom-[20%] m-auto transition-transform hover:scale-105 active:scale-95"
+        onClick={() => setWokes(true)}>
+        Start
+      </Button>
     </div>
   );
 }
