518eb7fe2ab3f80d1ecc4903c4f01b9725f0c00c
Alex Kadai <alexandrkadai@gmail.com>
2025-07-28 17:02:16 +0300

    Ballers

--- app/(no-layout)/ballers/page.tsx
+++ app/(no-layout)/ballers/page.tsx
@@ -1,16 +1,56 @@
+'use client';
 import Image from 'next/image';
-import welcome from '../../assets/welcome.jpg';
+import { useState } from 'react';
+import pusher from '../../assets/images/pusher.png';
+import puserOpened from '../../assets/images/pusherOpened.png';
 
-export default function page() {
+export default function Page() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const [stage, setStage] = useState(0);
+
+  const handleClick = () => {
+    if (stage < appleStages.length - 1) {
+      setStage(stage + 1);
+    }
+  };
+
+  const appleStages = [
+    '/apple1.png',
+    '/apple2.png',
+    '/apple3.png',
+    '/apple4.png',
+    '/apple5.png',
+  ];
+
   return (
-    <div className="h-full w-full bg-[url('/patheon.jpg')] bg-cover bg-center bg-no-repeat">
-      <div className="mt-32 flex flex-col items-center justify-center">
-        <Image src={welcome} width={150} height={50} alt="welcome" />
-        <span className="m-auto mt-20 text-3xl text-purple-800">TO</span>
+    <div className="relative h-screen w-full bg-[url('/background.png')] bg-cover bg-center bg-no-repeat overflow-hidden">
+      <Image
+        src={isHovered ? puserOpened : pusher}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        alt="Pusher Logo"
+        className="absolute right-[200px] top-[150px] h-[240px] w-[179px] rotate-[4deg] transform transition-all duration-300 ease-in-out"
+      />
+      <div className="absolute left-[444px] bottom-[-100px] h-[200px] w-[200px] cursor-pointer transition-transform duration-300 ease-in-out">
+        <Image
+          key={stage}
+          src={appleStages[stage]}
+          alt="Apple"
+          width={100}
+        height={120}
+        className="h-[120px] w-[100px] cursor-pointer transition-transform duration-300 ease-in-out"
+        onClick={handleClick}
+      />
       </div>
-      <h1 className="mt-[50px] text-center text-[144px] text-purple-800">
-        BALLERS
-      </h1>
+      <video
+    src="/videos/textDVD.mp4" 
+    className="absolute bottom-[75px] right-[417px] w-[170px] h-[105px] object-cover"
+    autoPlay
+    muted
+    loop
+    playsInline
+  />
     </div>
   );
 }
